21dfd121c13e44a1bb475c9d792348ea95afa3be
Elisee Othniel AMEGASSI <eliseothniel@gmail.com>
2020-07-06 11:18:17 +0000

    Enable cors and add 404 error and default index page
    	update :         README.MD
    	update :         app.js
    	update :         package.json
    	new file :  views/index.html

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const axios = require("axios");
 const moment = require("moment");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
+// Enable All CORS Requests
+app.use(cors());
+
+// Default
 app.get("/", (req, res) => {
-  res.send(
-    "<a href='/api/github/trending-languages'>/api/github/trending-languages</a>"
-  );
-  res.end();
+  res.sendFile(__dirname + "/views/index.html")
 });
 
+// Trending repositories languages endpoint
 app.get("/api/github/trending-languages", (req, res) => {
   let date = moment().subtract(30, "days").toISOString();
   axios
@@ -22,12 +24,14 @@ app.get("/api/github/trending-languages", (req, res) => {
     )
     .then((response) => {
       let repos = response.data.items;
-      let out = [];
+      let out = []; // res output
 
+      // Put the language in each repo in a set and remove null if any
       let languages = new Set();
       repos.map((repo) => languages.add(repo.language));
       languages.delete(null);
 
+      // Put each language in the ouput array along with the number of repos and the list of repos
       [...languages].map((lang) => {
         out.push({
           language: lang,
@@ -37,7 +41,8 @@ app.get("/api/github/trending-languages", (req, res) => {
         });
       });
 
-      function compare(a, b) {
+      // sort the output array by the number of repos for each language in descending order
+      out.sort((a, b) => {
         let comparison = 0;
         if (a.repositories_count > b.repositories_count) {
           comparison = -1;
@@ -45,15 +50,19 @@ app.get("/api/github/trending-languages", (req, res) => {
           comparison = 1;
         }
         return comparison;
-      }
-      
-      out.sort(compare);
+      });
 
       res.json(out);
     })
     .catch((err) => {
       throw err;
     });
+});
+
+// Not found middleware
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(404).send("not found");
 });
 
 const listener = app.listen(PORT, () => {
